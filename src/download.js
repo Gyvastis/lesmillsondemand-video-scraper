f88f281613801e552c4f383bd2b59ae915a0c4ac
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2020-03-21 11:24:37 +0100

    resume partially downloaded

--- src/download.js
+++ src/download.js
@@ -9,26 +9,14 @@ const readFile = Promise.promisify(fs.readFile);
 const readVideoIdsFromJson = () => readFile('./downloads/video_ids.json')
   .then(text => JSON.parse(text));
 
-const fileExists = fileName => {
-  try {
-    fs.accessSync(fileName, fs.constants.F_OK);
-    return true
-  }
-  catch(e) {
-    return false;
-  }
-};
-
 const downloadVideo = (title, url) => new Promise((resolve, reject) => {
   const fileName = `./downloads/${_.snakeCase(title)}.mp4`;
 
-  if(fileExists(fileName)) {
-    console.log(`File '${fileName} is already downloaded.'`);
-    resolve();
-    return;
-  }
-
-  const video = youtubedl(url, [], { cwd: __dirname });
+  const downloaded = fs.existsSync(fileName) ? fs.statSync(fileName).size : 0;
+  const video = youtubedl(url, [], {
+    start: downloaded,
+    cwd: __dirname
+  });
 
   video.on('info', function(info) {
     console.log(`Downloading '${title}' (${fileName})...`)

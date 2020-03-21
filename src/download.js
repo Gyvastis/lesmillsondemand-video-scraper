a720b016d7c8a5459966961b68ccf78e0fd93de3
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2020-03-21 13:40:49 +0100

    remove unused line

--- src/download.js
+++ src/download.js
@@ -3,14 +3,14 @@ const Promise = require('bluebird');
 const fs = require('fs');
 const youtubedl = require('youtube-dl');
 
-const concurrentDownloads = 5;
+const concurrentDownloads = 3;
 const readFile = Promise.promisify(fs.readFile);
 
 const readVideoIdsFromJson = () => readFile('./downloads/video_ids.json')
   .then(text => JSON.parse(text));
 
 const downloadVideo = (title, url) => new Promise((resolve, reject) => {
-  const fileName = `./downloads/${_.snakeCase(title)}.mp4`;
+  const fileName = `/Volumes/iSafe/LesMills/${_.snakeCase(title)}.mp4`;
 
   const downloaded = fs.existsSync(fileName) ? fs.statSync(fileName).size : 0;
   const video = youtubedl(url, [], {
@@ -32,8 +32,6 @@ const downloadVideo = (title, url) => new Promise((resolve, reject) => {
 
 (async () => {
   const videoIds = await readVideoIdsFromJson();
-
-  const { title, url } = videoIds[0];
 
   await Promise.map(videoIds, ({ title, url }) => downloadVideo(title, url), {
     concurrency: concurrentDownloads,
